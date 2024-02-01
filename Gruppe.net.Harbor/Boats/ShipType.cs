8e3f9f287a440d12643119333d39d1de26464b9a
D3S002 <abdulhma@hiof.no>
2024-02-01 13:18:04 +0100

    shipspeed

--- Gruppe.net.Harbor/Boats/ShipType.cs
+++ Gruppe.net.Harbor/Boats/ShipType.cs
@@ -1,9 +1,29 @@
-﻿namespace Gruppe.net.Harbor.Models
+﻿using Gruppe.net.Harbor.Models;
+
+namespace Gruppe.net.Harbor.Models
 {
     public enum ShipType
     {
         CARGO_SHIP,
         CRUISE_SHIP,
         LEISURE_BOAT
+    }
+
+    public class ShipSpeeds
+    {
+        public static int GetSpeed(ShipType shipType)
+        {
+            switch (shipType)
+            {
+                case ShipType.CARGO_SHIP:
+                    return "shipSpeed" = 20; 
+                case ShipType.CRUISE_SHIP:
+                    return "shipSpeed" = 30; 
+                case ShipType.LEISURE_BOAT:
+                    return "shipSpeed" = 50;
+                default:
+                    return null;
+            }
+        }
     }
 }
